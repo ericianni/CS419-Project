daf124fba7e7a52de5faafd312e112bf4297dbfa
ericianni <ebianni@gmail.com>
2015-08-13 14:17:37 -0400

    SQL testing

--- alchemy_test.py
+++ alchemy_test.py
@@ -12,7 +12,8 @@ def init_sql():
     db_name = raw_input("Enter database name: ")
     usr = raw_input('Enter username: ')
     pw = getpass('Enter password: ')
-    db_string = "postgresql+psycopg2://{0}:{1}@/{2}".format(usr, pw, db_name)
+    #db_string = "postgresql+psycopg2://{0}:{1}@/{2}".format(usr, pw, db_name)
+    db_string = "mysql+mysqldb://{0}:{1}@/{2}".format(usr, pw, db_name)
     print db_string
     engine = create_engine(db_string)
     return engine
@@ -30,8 +31,14 @@ def display_menu():
     print "4. Delete Table"
     print "5. Select All Data From Table"
     print "6. Remove Row From Table"
+    print "7. Run SQL Command"
     print "0. Exit"
 
+def run_sql(engine):
+    sql = raw_input("Enter SQL command: ")
+    con = engine.connect()
+    con.engine.execute(sql)
+    
 def create_table(engine):
     table_name = raw_input("Enter table name: ")
     num_col = raw_input("Enter # of columns: ")
@@ -44,7 +51,7 @@ def create_table(engine):
     
     table = Table(str(table_name), metadata,
                   Column('id', Integer, primary_key=True, autoincrement=True),
-                  *(Column(name, data_type) for name, data_type in cols.iteritems()))
+                  *(Column(name, data_type(20)) for name, data_type in cols.iteritems()))
     
     metadata.create_all(engine)
 
@@ -105,6 +112,7 @@ menu_options = { 1 : display_tables,
 		 4 : delete_table,
 		 5 : select_all,
                  6 : remove_from_table,
+                 7 : run_sql,
                  0 : shutdown}
 
 def main():
